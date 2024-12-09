39a23b85717a7dfafe59e6b5ffa43f00ebcb80cb
MongiSaad <94915480+MongiSaad@users.noreply.github.com>
2024-12-09 02:35:14 +0100

    Update Controller.java

--- userManagerMS/src/main/java/org/INSA/userManagerMS/controller/Controller.java
+++ userManagerMS/src/main/java/org/INSA/userManagerMS/controller/Controller.java
@@ -13,14 +13,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class Controller {
     
-	@Value("${bdd.url}")
-    private String bddUrl;
-
-    @Value("${bdd.username}")
-    private String bddLogin;
-
-    @Value("${bdd.password}")
-    private String bddMdp;
+    private String bddUrl = jdbc:mysql://srv-bdens.insa-toulouse.fr:3306/projet_gei_062;
+    private String bddLogin = projet_gei_062;
+    private String bddMdp = Uph3Quie;
 
     // Ajouter un utilisateur
     @PostMapping("/add")
